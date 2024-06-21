d479fd472c64b43d6ca4569e4a910655286bea7d
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-21 15:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let form=document.getElementByID("form1");
+	var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
+	alert(firstName + ' ' + lastName);
 
 }
